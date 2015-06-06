3ee42fe87ad8afefab0647e92e60e5ce76e4526d
nikitosl <altuxov.nikita@gmail.com>
2015-06-06 22:21:42 +0300

    почти готово_2

--- JS/func_prorisovka_grafa.js
+++ JS/func_prorisovka_grafa.js
@@ -1,117 +1,179 @@
+
+
+
+
+function graf_painting(mas, size, color) {
+    for (var i = 0; i < size; i++) {
+        if (mas[i].vis == true)
+            color = '#066';
+        else {
+            color = '#600';
+            mas[i].q1.way = 1;
+            mas[i].q2.way = 1;
+            f = 0;
+            for (j = 0; j < size && f == 0; j++) {
+                if ((mas[j].q1.num == mas[i].q1.num && mas[j].vis == true) || (mas[j].q2.num == mas[i].q1.num && mas[j].vis == true))
+                    f = 1;
+            }
+            if (f == 0)
+                mas[i].q1.way = 0;
+
+            f = 0;
+            for (j = 0; j < size && f == 0; j++) {
+                if ((mas[j].q1.num == mas[i].q2.num && mas[j].vis == true) || (mas[j].q2.num == mas[i].q2.num && mas[j].vis == true))
+                    f = 1;
+            }
+            if (f == 0)
+                mas[i].q2.way = 0;
+
+
+
+        }
+        edge_painting(mas, i,size, color);
+        node_painting(mas, i, size, color);
+       // console.log(mas[i].q1.num, ':', mas[i].q1.way, ';', mas[i].q2.num, ':', mas[i].q2.way)
+    }
+}
+
+
+function node_painting(mas, i,k, col) {
+    var x1, y1, x2, y2, j;
+    var radius = 5;
+    var col1=col, col2=col;
+    
+
+    for (j = 0; j < k; j++) {
+        if (mas[j].q1.num == mas[i].q1.num) mas[j].q1.way = mas[i].q1.way;
+        else if (mas[j].q2.num == mas[i].q1.num) mas[j].q2.way = mas[i].q1.way;
+        else if (mas[j].q1.num == mas[i].q2.num) mas[j].q1.way = mas[i].q2.way;
+        else if (mas[j].q2.num == mas[i].q2.num) mas[j].q2.way = mas[i].q2.way;
+
+    }
+
+    if (mas[i].q1.way == 1) col1 = '#AA0';
+    else if (mas[i].q1.way == 0) col1 = '#600';
+    if (mas[i].q2.way == 1) col2 = '#AA0';
+    else if (mas[i].q2.way == 0) col2 = '#600';
+
+
+    jc.start('can');
+    x1 = mas[i].q1.x + radius; x2 = mas[i].q2.x + radius; y1 = mas[i].q1.y + radius; y2 = mas[i].q2.y + radius;
+    jc.circle(x1, y1, radius - 1, col1, 1);
+    jc.text(mas[i].q1.num, x1, y1 + 15, col1);
+    jc.circle(x2, y2, radius - 1, col2, 1);
+    jc.text(mas[i].q2.num, x2, y2 + 15, col2);
+    jc.start('can', true);
+
+}
+
+
+function matrix_painting(matrix,size, k)
+{
+    jc.start('tab');
+    jc.clear('tab');
+    var i, j, x=0,y=0;
+    for (i = 0; i < size; i++) {
+        x = 0;
+        for (j = 0; j < size; j++) {
+            //jc.text(matrix[k], x, y, '#066');
+   //         if (matrix[i][0] == 1) jc.text('4len', x, y, '#A00');
+   //         else jc.text('4len', x, y, '#066');
+            x += 30;
+        }
+        y += 30;
+    }
+    jc.start('tab');
+}
 // JavaScript source code
-function prorisovka(mas) //mas - массив ребер формата: [q1{num,x,y}, -первая вершина
+function edge_painting(mas, i,k, col) //mas - массив ребер формата: [q1{num,x,y}, -первая вершина
     //                             q2{num,x,y}, -вторая вершина
     //                             weight,      -вес ребра
     //                             print];      -нарисовано ли ребро
     //                                             (1/0)
 {
-
-    var x1, y1, x2, y2;
+    if (!mas[i])
+        return null;
+    var x1, y1, x2, y2,f;
     var radius = 5;
-    var col = '#066';               //невыбраные линии
+    //  var col = '#066';               //невыбраные линии
     var col0 = 'rgba(0,6,6,0.04)';  //невыбраные прямоугольники
     var col1 = 'rgba(0,9,9,0.2)';   //подсвеченые прямоугольники
-    var col2 = '#600';              //выбраные линии
+    var col2 = '#600';//выбраные линии
     var kos = [];
     var wei = [];
     var center = [];
-
+    var sr_x=0, sr_y=0, koef;
     jc.start('can');
-    jc.clear('can');
+    //jc.clear('can');
     //var point;
-    for (var i = 0; i < k; i++) {
-        // jc.circle(100, 100, 50, 'rgba(0,0,0,1)', 1);
-        x1 = mas[i].q1.x + radius; x2 = mas[i].q2.x + radius; y1 = mas[i].q1.y + radius; y2 = mas[i].q2.y + radius;
-
-        if (x1 < x2) {
-            jc.line([[x1 - 3, y1 + 3], [x2 - 3, y2 + 3], [x2 + 3, y2 - 3], [x1 + 3, y1 - 3]], col0, 1)
-                .name(i + 1)
-                .lineStyle({ lineWidth: 1 })
-                .mousemove(function () {
-                    this.color(col1);
-                    console.log(wei[this.name()]);
-                    jc('#speshial_rect' + this.name()).visible(true);
-                    jc('#speshial_text' + this.name()).visible(true);
-                })
-                 .mouseout(function () {
-                     this.color(col0);
-                     console.log('Clearing');
-                     jc('#speshial_rect' + this.name()).visible(false);
-                     jc('#speshial_text' + this.name()).visible(false);
-                 })
-                .click(function () {
-                    if (kos[this.name()] == 0) {
-                        jc('#' + this.name() + '1').color(col2);
-                        jc('#speshial_text' + this.name()).color(col2);
-                        kos[this.name()] = 1;
-                    }
-                    else {
-                        jc('#' + this.name() + '1').color(col);
-                        jc('#speshial_text' + this.name()).color(col);
-                        kos[this.name()] = 0;
-                    }
-                });
-        }
-            //********************************************************************
-        else {
-            jc.line([[x1 - 3, y1 - 3], [x2 - 3, y2 - 3], [x2 + 3, y2 + 3], [x1 + 3, y1 + 3]], col0, 1)
-                .name(i + 1)
-                .lineStyle({ lineWidth: 1 })
-                .mousemove(function () {
-                    this.color(col1);
-                    console.log(wei[this.name()]);
-                    jc('#speshial_rect' + this.name()).visible(true).up();
-                    jc('#speshial_text' + this.name()).visible(true).up();
-                })
-                 .mouseout(function () {
-                     this.color(col0);
-                     console.log('Clearing');
-                     jc('#speshial_rect' + this.name()).visible(false);
-                     jc('#speshial_text' + this.name()).visible(false);
-                 })
-                .click(function () {
-                    if (kos[this.name()] == 0) {
-                        jc('#' + this.name() + '1').color(col2);
-                        jc('#speshial_text' + this.name()).color(col2);
-                        kos[this.name()] = 1;
-                    }
-                    else {
-                        jc('#' + this.name() + '1').color(col);
-                        jc('#speshial_text' + this.name()).color(col);
-                        kos[this.name()] = 0;
-                    }
-                });
-        }
-        jc.line([[x1, y1], [x2, y2]], col)
-            .id(i + 1 + '1');
-
-        kos[i + 1] = 0;
-
-
-        center[i + 1] = jc('#' + (i + 1) + '1').getCenter();
-        jc.rect(center[i + 1].x - 10, center[i + 1].y - 10, 20, 20, '#fff', 1)
-            .id('speshial_rect' + (i + 1))
-            .visible(false)
+    //   for (var i = 0; i < k; i++) {
+    // jc.circle(100, 100, 50, 'rgba(0,0,0,1)', 1);
+    x1 = mas[i].q1.x + radius; x2 = mas[i].q2.x + radius; y1 = mas[i].q1.y + radius; y2 = mas[i].q2.y + radius;
+    /*
+    if (x1 < x2) sr_x = x1 / x2;
+    else sr_x = x2 / x1;
+    if (y1 < y2) sr_y = y1 / y2;
+    else sr_y = y2 / y1;
+    koef=2;
+    sr_x *=  koef; sr_x += 3;
+    sr_y *= koef; sr_y += 2;   
+    */
+    if (x1 < x2 && y1 > y2) {
+        sr_x += 3;
+        sr_y += 3;
+    }
+    else if (x1 > x2 && y1 < y2) {
+        sr_x = 3;
+        sr_y = 3;
+    }
+    else {
+        sr_x += 3;
+        sr_y += 3;
+    }
+  /*  sr_x += 30;
+    sr_y += 30;*/
+        jc.line([[x1 - sr_x, y1 + sr_y], [x2 - sr_x, y2 + sr_y], [x2 + sr_x, y2 - sr_y], [x1 + sr_x, y1 - sr_y]], col0, 1)
+            .name(i + 1)
+            .lineStyle({ lineWidth: 1 })
             .mousemove(function () {
-                js.pause();
+                this.color(col1);
+                //console.log(wei[this.name()]);
+                jc('#speshial_rect' + this.name()).visible(true).up();
+                jc('#speshial_text' + this.name()).visible(true).up();
+            })
+             .mouseout(function () {
+                 this.color(col0);
+                 //console.log('Clearing');
+                 jc('#speshial_rect' + this.name()).visible(false);
+                 jc('#speshial_text' + this.name()).visible(false);
+             })
+            .click(function () {
+                if (mas[this.name()-1].vis == true) {
+                    jc('#' + this.name() + '1').color(col2);
+                    jc('#speshial_text' + this.name()).color(col2);
+                    mas[this.name() - 1].vis = false;
+
+                  //  node_painting(mas, i,k, col);
+                }
+                else  {
+                    jc('#' + this.name() + '1').color(col);
+                    jc('#speshial_text' + this.name()).color(col);
+                    mas[this.name() - 1].vis = true;
+                 //   node_painting(mas, i,k, col);
+               }
             });
 
-        jc.text(mas[i].weight, center[i + 1].x - 10, center[i + 1].y + 5, col, 1)
-            .id('speshial_text' + (i + 1))
-            .visible(false)
-            .mousemove(function () {
-                js.pause();
-            });
+    jc.line([[x1, y1], [x2, y2]], col)
+        .id(i + 1 + '1');
+
+    center[i + 1] = jc('#' + (i + 1) + '1').getCenter();
+    jc.rect(center[i + 1].x - 10, center[i + 1].y - 10, 20, 20, '#fff', 1)
+        .id('speshial_rect' + (i + 1))
+        .visible(false);
 
 
-
-        jc.circle(x1, y1, radius - 1, col, 1);
-        jc.text(mas[i].q1.num, x1, y1 + 15, col);
-        jc.circle(x2, y2, radius - 1, col, 1);
-        jc.text(mas[i].q2.num, x2, y2 + 15, col);
-        jc.start('can', true);
-    }
-    for (i = 0; i < k; i++) {
-        mas[i].print = kos[i];
-    }
+    jc.text(mas[i].weight, center[i + 1].x - 10, center[i + 1].y + 5, col, 1)
+        .id('speshial_text' + (i + 1))
+        .visible(false);
+    jc.start('can', true);
 }
--- JS/graf.js
+++ JS/graf.js
@@ -9,28 +9,36 @@ $(document).ready(function () {
 */
 
 //*** создание МАССИВА
+
+var count = 0;
 function NewReb(num1, x1, y1, num2, x2, y2, weighta) {
 
     var reb = {
         q1: {
             num: num1,
             x: x1,
-            y: y1
+            y: y1,
+            way: 2
         },
         q2: {
             num: num2,
             x: x2,
-            y: y2
+            y: y2,
+            way: 2
         },
         weight: weighta,
-        print: false
+        vis:true
     }
     return reb;
 }
+
+
+
 var k = 0;
 var mas = [] ;
+var i;
 mas.push();
-mas[0] = NewReb(1, 30, 180, 2, 40, 0, 91);
+mas[0] = NewReb(1, 30, 180, 2, 40, 0, 91,0);
 
 mas.push();
 mas[1] = NewReb(4, 0, 50, 3, 70, 90, 90);
@@ -41,11 +49,162 @@ mas[3] = NewReb(2, 40, 0, 4, 0, 50, 100);
 mas.push();
 mas[4] = NewReb(2, 40, 0, 5, 0, 150, 20);
 mas.push();
-mas[5] = NewReb(8, 280, 100, 4, 0, 50, 100);
+mas[5] = NewReb(6, 280, 100, 4, 0, 50, 120);
 mas.push();
-mas[6] = NewReb(2, 40, 0, 5, 6, 0, 0);
+mas[6] = NewReb(6, 280, 100, 5, 0, 150, 55);
 
 k = 7;
-console.log(mas);
-prorisovka(mas);
+var matrix = [];
+var steck = [];
+var g = 0;
+for (i = 0; i < k - 1; i++)
+    steck[i] = NewReb(0, 0, 0, 0, 0, 0, 0);
 
+graf_painting(mas, k, '#066');
+prim(mas, k);
+console.log('steck:',steck);
+console.log('g:',g);
+console.log('matrix:', matrix);
+
+
+function next() {
+
+    var color,f,j;
+    if (count < g) {
+        //while (steck[count].vis==false) count++;  // Не проверено
+       
+        steck[count].vis = false;
+        jc.clear('can');
+        graf_painting(mas, k, '#066');
+        graf_painting(steck, g, '#066');
+        matrix_painting(matrix, k, count);
+        count++;
+        
+
+
+    }
+    else
+        alert("Building is finished!");
+}
+function back() {
+    if (count > 0)
+    {
+        count--;
+        steck[count].vis = true;
+        steck[count].q1.way = 2;
+        steck[count].q2.way = 2;
+        jc.clear('can');
+        graf_painting(mas, k, '#066');
+        graf_painting(steck, g, '#066');
+       
+    }  
+}
+function restart()
+{
+
+        for (var i = 0; i < g; i++)
+        {
+            steck[i].vis = true;
+            steck[i].q1.way = 2;
+            steck[i].q2.way = 2;
+        }
+        jc.clear('can');
+        count = 0;
+        graf_painting(mas, k, '#066')
+    
+}
+
+function prim(mas, size)
+{
+    var tabl = [];
+    //var tabl1 = [];
+    var i, j;
+    var min = { x: 0, y: 0 };
+    var empty = [];
+    var stolb = [];
+//********** NEW Matrix *****************
+    for (i = 0; i < size; i++)
+    {
+        var tabl1 = [];
+        for (j = 0; j < size; j++)
+            tabl1[j] = 0;
+        tabl[i]= tabl1;
+    }
+    for (i = 0; i < size; i++)
+        empty[i] = 0;
+    
+    for (i = 0; i < size; i++)
+        stolb[i] = 0;
+
+    for (i=0; i<size; i++)
+    {
+        tabl[mas[i].q1.num][mas[i].q2.num] = mas[i].weight;
+        tabl[mas[i].q2.num][mas[i].q1.num] = mas[i].weight;
+    }
+    //for (i = 0; i < size; i++) console.log(tabl[i])
+    
+
+//*******************************************
+    f = 1;
+    var x = 0, y = 0;
+     while (f==1)
+     {
+        
+        min = min_from_table(tabl, size, stolb);
+       // console.log('stolb:',stolb);
+        stolb[min.x] = 1;
+        stolb[min.y]=1;
+        stolb[0]=1;
+       // console.log(min.x, min.y, tabl[min.x][min.y]);
+        
+        tabl[min.x] = empty;
+        tabl[min.y] = empty;
+
+        for (i = 0; i < size; i++)
+            if ((mas[i].q1.num== min.x || mas[i].q1.num== min.y) && (mas[i].q2.num == min.x || mas[i].q2.num == min.y))
+            {
+                steck[g] = mas[i];
+               // console.log('4len');
+                g++;
+            }
+
+        matrix[g] = copy(tabl);
+       // console.log('g:',g);
+      //  console.log(matrix);
+ /*       jc.start('tab');
+        jc.clear('tab');
+        x = 0; y = 0;
+        
+*/
+    }
+
+}
+
+function min_from_table(mas, k, stolb)
+{
+    var mx = 0, my = 0, i, j;
+    f=0;
+    for (i = 0; i < k; i++)
+        for (j = 0; j < k; j++)
+            if (mas[i][j] != 0 && (stolb[i] ==1 || stolb[j]==1 || stolb[0]==0) && f==0 ) {
+                mx = i;
+                my = j;
+                f = 1;
+              //  console.log('1) mx:', mx, 'my:', my);
+            }
+    for(i=0;i<k;i++)
+        for(j=0;j<k;j++)
+            if ((mas[i][j] < mas[mx][my]) && (mas[i][j]!=0) && (stolb[i] ==1 || stolb[j]==1 || stolb[0]==0))
+            {
+              //  console.log('2) stole:', stolb[i],  stolb[j], stolb[0]);
+                mx = i; my = j;
+              //  console.log('2) mx:', mx, 'my:', my);
+            }
+    return {x: mx, y: my};
+}
+
+function copy(table)
+{
+    var m = table;
+    return m;
+}
